4ca36ded2eeb6f15dccd46b3f2f5744808f4e94b
renandcr <renandamagazine@gmail.com>
2022-10-28 15:39:03 -0300

    feat: Criar componente Button. Editar componente Home.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Header";
 import { MainHomeContainer } from "./style";
+import { HomeContainer } from "./style";
 
 const Home: React.FC = (): JSX.Element => {
   return (
     <>
       <Header />
       <MainHomeContainer></MainHomeContainer>
+      <HomeContainer></HomeContainer>
     </>
   );
 };
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -2,4 +2,18 @@ import styled from "styled-components";
 
 export const MainHomeContainer = styled.main`
   display: flex;
+  justify-content: center;
+  padding-bottom: 30px;
+`;
+
+export const HomeContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 1440px;
+  margin-top: 125px;
+
+  @media only screen and (min-width: 650px) {
+    margin-top: 100px;
+  }
 `;
