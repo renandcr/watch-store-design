9def7abde98dc27b7d5a74f6bfa496a556f41638
renandcr <renandamagazine@gmail.com>
2022-10-30 16:34:32 -0300

    feat: Criar componente OrderSummary. Editar componente CartPage.

---
+++ src/components/OrderSummary/index.tsx
@@ -0,0 +1,36 @@
+import {
+  PaymentInformationContainer,
+  OrderSummaryContainer,
+  KeepBuyingContainer,
+  OrderBodyContainer,
+  TitleContainer,
+} from "./style";
+
+import { BsBagFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import Button from "../Button";
+
+const OrderSummary: React.FC = (): JSX.Element => {
+  return (
+    <OrderSummaryContainer>
+      <TitleContainer>
+        <h2>Resumo do pedido</h2>
+      </TitleContainer>
+      <OrderBodyContainer>
+        <PaymentInformationContainer>
+          <span>1 unidades</span>
+          <span>R$ 2.890</span>
+        </PaymentInformationContainer>
+        <Link to="/">
+          <KeepBuyingContainer>
+            <BsBagFill className="bag_icon" />
+            Continuar comprando
+          </KeepBuyingContainer>
+        </Link>
+        <Button>Finalizar pedido</Button>
+      </OrderBodyContainer>
+    </OrderSummaryContainer>
+  );
+};
+
+export default OrderSummary;
--- src/pages/CartPage/style.ts
+++ src/pages/CartPage/style.ts
@@ -17,10 +17,15 @@ export const CartPageContainer = styled.div`
     margin-top: 100px;
   }
   @media only sreen and (min-width: 768px) {
+    flex-direction: row-reverse;
+    align-items: flex-start;
+    justify-content: center;
   }
   @media only sreen and (min-width: 1024px) {
+    column-gap: 70px;
   }
   @media only sreen and (min-width: 1440px) {
+    column-gap: 140px;
   }
 `;
 
@@ -34,5 +39,16 @@ export const CartCardsSection = styled.section`
   @media only screen and (min-width: 768px) {
     width: 50%;
     max-width: 550px;
+  }
+`;
+
+export const OrderSummarySection = styled.section`
+  width: 90%;
+  max-width: 410px;
+  margin-bottom: 30px;
+
+  @media only screen and (min-width: 768px) {
+    max-width: 360px;
+    margin-left: 20px;
   }
 `;
