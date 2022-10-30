48dd45ae25273d3edd4bb96aa4b6920073e1d63d
renandcr <renandamagazine@gmail.com>
2022-10-30 10:13:56 -0300

    feat: Criar componente CartProductCard.

---
+++ src/components/CartProductCard/index.tsx
@@ -0,0 +1,49 @@
+import {
+  CartProductCardContainer,
+  CartImageContainer,
+  CartDescriptionContainer,
+  TrashContainer,
+  AddAndSubtractComponent,
+  AddContainer,
+  QuantityContainer,
+  SubtractContainer,
+  UnitsContainerAndDeletion,
+} from "./style";
+
+import Watch from "../../assets/images/eight.jpg";
+import { RiSubtractLine } from "react-icons/ri";
+import { IoIosAdd } from "react-icons/io";
+import { FaTrash } from "react-icons/fa";
+
+const CartProductCard: React.FC = (): JSX.Element => {
+  return (
+    <CartProductCardContainer>
+      <CartImageContainer>
+        <img src={Watch} alt="Imagem ilustrativa de um relógio" />
+      </CartImageContainer>
+      <CartDescriptionContainer>
+        <h2>Aqui fica a descrição do relógio </h2>
+        <span>1.990</span>
+      </CartDescriptionContainer>
+
+      <UnitsContainerAndDeletion>
+        <AddAndSubtractComponent>
+          <AddContainer>
+            <IoIosAdd />
+          </AddContainer>
+          <QuantityContainer>1</QuantityContainer>
+          <SubtractContainer>
+            <RiSubtractLine />
+          </SubtractContainer>
+        </AddAndSubtractComponent>
+        <TrashContainer>
+          <span>
+            <FaTrash />
+          </span>
+        </TrashContainer>
+      </UnitsContainerAndDeletion>
+    </CartProductCardContainer>
+  );
+};
+
+export default CartProductCard;
